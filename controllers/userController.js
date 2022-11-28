062f33c086764704ecb1e5070abc4e6a14d889a8
Dempoleon <dempoleon@gmail.com>
2022-11-27 21:44:46 -0800

    added some code to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,36 +1,36 @@
-const {ObjectId} = require('mongoose').Types;
-const {User, Thought} = require('../models');
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
 
-
+//todo: aggregate functions
 
 module.exports = {
     // get all users
     getAllUsers(req, res) {
         User.find()
-        .then(async (users) => {
-            const userOjb = {
-                users,
-            };
-            return res.json(userOjb);
-        })
-        .catch((err) => {
-            console.log(err)
-            return res.status(500).json(err);
-        });
+            .then(async (users) => {
+                const userOjb = {
+                    users,
+                };
+                return res.json(userOjb);
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).json(err);
+            });
     },
 
     // get single user with thoughts and friend data
     // todo: get the thoughts and friends data, might have to use aggregate function
     getUser(req, res) {
-        User.findOne({_id: req.params.userId})
-        .select('-__v')
-        .then(async (user) => {
-            !user
-                ? res.status(404).json({message: 'No user found with that ID'})
-                : res.json({
-                    user,
-                })
-        })
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then(async (user) => {
+                if (!user) {
+                    res.status(404).json({ message: 'User not found' })
+                } else {
+                    res.json({ user })
+                }
+            })
     },
 
     // user post route
@@ -40,5 +40,34 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
 
-    
+    // delete route to remove single user
+    deleteUser(req, res) {
+        User.findOneAndRemove({ _id: req.params.userId })
+            .then((user) => {
+                if (!user) {
+                    res.status(404).json({ message: 'User not found' })
+                } else {
+                    //todo: delete associated thoughts when user is deleted
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+    // put route to update single user
+    updateUser(req, res) {
+        User.findOneAndUpdate({_id: req.params.userId})
+            .then((user) => {
+                if(!user){
+                    res.status(404).json({message: 'User not found'})
+                } else {
+                    // todo: update associated thoughts
+                }
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            });
+    }
 }
